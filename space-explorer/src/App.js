1d1b719f02b02332574b07dd9172892f61e63f0e
denga723 <denga723@newschool.edu>
2024-11-19 19:57:23 -0500

    Update App.js

--- space-explorer/src/App.js
+++ space-explorer/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Planet from "./components/Planet";
+import Footer from "./components/Footer";
+import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Space Explorer</h1>
+      <Planet />
+      <Footer />
     </div>
   );
 }
